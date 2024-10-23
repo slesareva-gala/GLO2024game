a705d729c727b0cdc6ab82b5fbd6e27760efd2f7
Галина Слесарева <galaslesareva@gmail.com>
2024-10-23 11:52:19 +0300

    add Задание урока 13

--- js/script.js
+++ js/script.js
@@ -1,29 +1,64 @@
-// Урок 11 Обработка событий
+// Урок 13 ToDo
 
 "use strict";
 
-const btn = document.getElementById("btn")
-const elRange = document.getElementById("range")
-const elRandeSpan = document.getElementById("range-span")
-const elText = document.getElementById("text")
-const elSquare = document.getElementById("square")
-const elCircle = document.getElementById("circle")
+const todoControl = document.querySelector('.todo-control')
+const headerInput = document.querySelector('.header-input')
+const todoList = document.querySelector('.todo-list')
+const todoCompleted = document.querySelector('.todo-completed')
 
-
-btn.onclick = () => {
-    elSquare.style.backgroundColor = elText.value.trim()
+const storage = {
+    is: localStorage.getItem('toDoData') ? true : false,
+    set: (arr) => localStorage.setItem('toDoData', JSON.stringify(arr)),
+    get: () => storage.is ? JSON.parse(localStorage.getItem('toDoData')) : []
 }
 
-elRange.addEventListener("input", (e) => {
-    const proc = e.target.value
+const render = () => {
+    todoList.innerHTML = ''
+    todoCompleted.innerHTML = ''
 
-    elRandeSpan.innerText = proc
-    elCircle.style.width = `${proc}%`
-    elCircle.style.height = `${proc}%`
+    storage.set(toDoData)
+
+    toDoData.forEach((item, index) => {
+        const li = document.createElement('li')
+        li.classList.add('todo-item')
+        li.innerHTML = `<span class="text-todo">${item.text}</span>
+        <div class="todo-buttons">
+			<button class="todo-remove"></button>
+			<button class="todo-complete"></button>
+		</div>
+        `
+        if (item.completed) todoCompleted.append(li)
+        else todoList.append(li)
+
+        li.querySelector('.todo-complete').addEventListener('click', () => {
+            item.completed = !item.completed
+            render()
+        })
+        li.querySelector('.todo-remove').addEventListener('click', () => {
+            toDoData.splice(index, 1)
+            render()
+        })
+    })
+}
+
+todoControl.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const newToDo = {
+        text: headerInput.value.trim().replace(/(  )+/g, " "),
+        completed: false
+    }
+
+    if (newToDo.text) {
+        toDoData.push(newToDo)
+        headerInput.value = ''
+
+        render()
+    }
 })
 
-document.getElementById("e_btn").style.display = "none"
-elRange.value = 0
-elCircle.style.width = "0%"
-elCircle.style.height = "0%"
+const toDoData = storage.get()
+render()
+
 
